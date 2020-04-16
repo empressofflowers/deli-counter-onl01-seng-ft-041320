b99f670f31d3ada77919fd9eeba476d08eb532c8
Devone Austin <devone.austin@gmail.com>
2020-04-16 23:55:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,10 @@ def line(q)
     puts "The line is currently: #{numbered.join(" ")}"
   end
 end
+
 def take_a_number(q, name)
   q.push(name)
   place = q.index(name)
    puts "Welcome, #{name}. You are number #{place+1} in line."
-end
+ end
 end
