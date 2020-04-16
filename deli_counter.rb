124c772f6163c84d1ca36e97e03dbae0ed24b8d3
Devone Austin <devone.austin@gmail.com>
2020-04-16 23:50:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,7 @@
 def line(q)
   if q.size == 0
     puts "The line is currently empty."
-  else
-    numbered = []
+  else numbered = []
     q.each_with_index {|name, index| numbered <<"#{index+1}. #{name}"}
     puts "The line is currently: #{numbered.join(" ")}"
   end
